6e8c6992fc93e4d8d607d8d7df516083832d838e
Luke Peacock <lukepeacock13@gmail.com>
2020-02-28 21:39:14 +0000

    added score manager|

---
+++ ShefJamVII/Assets/ScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+	public float score = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {	
+        score = 0.0f;
+        InvokeRepeating("updateScoreTime", 0, 1.0f);
+    }
+
+
+    void updateScoreTime(){
+    	score += 10;
+    }
+
+    void updateScore(int i){
+    	score += i;
+    }
+
+    void OnGUI(){
+  
+    	GUI.BeginGroup (new Rect(5, 5, 200.0f, 20));
+		GUI.Box (new Rect (0, 0, 200.0f, 20.0f), "Score: " + score);
+		GUI.EndGroup ();
+    }
+
+
+
+}
